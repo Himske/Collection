30afcbe0a9288698da81f5e5c2862c135c27ce52
fredrik-nilsson18 <fredrik.nilsson18@ikea.com>
2019-11-26 16:20:00 +0100

    Added database and Book class

--- flask_collection.py
+++ flask_collection.py
@@ -3,11 +3,33 @@ Docstring
 '''
 
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import BookForm
 
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = 'c1f527892b63e6654766d74a389ffdf9'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    author = db.Column(db.String(50), nullable=False)
+    title = db.Column(db.String(50), nullable=False)
+    sub_title = db.Column(db.String(50))
+    volume = db.Column(db.Integer)
+    publication_date = db.Column(db.DateTime)
+    print_format = db.Column(db.String(20))
+    pages = db.Column(db.Integer)
+    publisher = db.Column(db.String(20))
+    cover = db.Column(db.String(20))
+    language = db.Column(db.String(20))
+    language_iso = db.Column(db.String(2))
+    isbn_13 = db.Column(db.String(13))
+    isbn_10 = db.Column(db.String(10))
+    description = db.Column(db.Text)
+
+    def __repr__(self):
+        return f'{self.title} written by {self.author}'
 
 books = [
     {
