0266f711b2bc251803fb7e77bff8aac9c4a17ade
fredrik-nilsson18 <fredrik.nilsson18@ikea.com>
2019-11-27 16:53:36 +0100

    Started adding user functionality

--- flask_collection/__init__.py
+++ flask_collection/__init__.py
@@ -4,10 +4,12 @@ Docstring
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'c1f527892b63e6654766d74a389ffdf9'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
-from flask_collection import routs
+from flask_collection import routes
--- flask_collection/forms.py
+++ flask_collection/forms.py
@@ -3,8 +3,13 @@ Docstring
 '''
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, DateField, FileField, SubmitField, TextAreaField
-from wtforms.validators import DataRequired, Length, NumberRange, Optional
+from flask_wtf.file import FileField, FileAllowed
+from wtforms import StringField, IntegerField, DateField, SubmitField, TextAreaField, \
+    PasswordField, BooleanField
+from wtforms.validators import DataRequired, Length, NumberRange, Optional, Email, EqualTo, \
+    ValidationError
+from flask_login import current_user
+from flask_collection.models import User
 
 class BookForm(FlaskForm):
     author = StringField('Author', validators=[DataRequired(), Length(min=2, max=50)])
@@ -15,10 +20,78 @@ class BookForm(FlaskForm):
     print_format = StringField('Format', validators=[Optional(), Length(min=2, max=20)])
     pages = IntegerField('Pages', validators=[Optional(), NumberRange(max=2000)])
     publisher = StringField('Publisher', validators=[Optional(), Length(min=2, max=20)])
-    cover = FileField('Cover', validators=[Optional()])
+    cover = FileField('Cover', validators=[FileAllowed(['jpg', 'png']), Optional()])
     language = StringField('Language', validators=[Optional()])
     language_iso = StringField('ISO Language', validators=[Optional()])
     isbn_13 = StringField('ISBN-13', validators=[Optional()])
     isbn_10 = StringField('ISBN-10', validators=[Optional()])
     description = TextAreaField('Description')
     submit = SubmitField('Add Book')
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('That email is taken. Please choose a different one.')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
+
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('That username is taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('That email is taken. Please choose a different one.')
+
+
+class RequestResetForm(FlaskForm):
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    submit = SubmitField('Request Password Reset')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is None:
+            raise ValidationError('There is no account with that email. You must register first.')
+
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Reset Password')
