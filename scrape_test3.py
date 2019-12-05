b33bdd64e340eff12489872140e786129f8c0738
fredrik-nilsson18 <fredrik.nilsson18@ikea.com>
2019-12-05 15:48:39 +0100

    Updated scraping script

--- scrape_test3.py
+++ scrape_test3.py
@@ -14,7 +14,7 @@ SEARCH_URL = 'https://us.nicebooks.com/search'
 # PARAMS = {'isbn': '9781779501202'} # Doomsday Clock Part 1
 # PARAMS = {'isbn': '9781401220884'} # Superman : Brainiac
 # PARAMS = {'q': '9789171197078'}
-PARAMS = {'q': '9789176424315'}
+PARAMS = {'q': '9789189128316'}
 
 HEADERS = {
     'Accept': 'text/html, application/xhtml+xml, application/xml; q=0.9, */*; q=0.8',
@@ -28,86 +28,113 @@ HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36 Edge/17.17134'
 }
 
-RESPONSE = SESSION.get(SEARCH_URL, params=PARAMS, headers=HEADERS)
 
-RESPONSE.html.render()
+def get_soup():
+    response = SESSION.get(SEARCH_URL, params=PARAMS, headers=HEADERS)
+    response.html.render()
+    # soup = bs.BeautifulSoup(RESPONSE.text, 'lxml')
+    soup = bs.BeautifulSoup(response.html.html, 'html5lib')
+    try:
+        book_url = 'https://us.nicebooks.com' + soup.find('a', attrs={'class': 'title'}).get('href')
+        response = SESSION.get(book_url, headers=HEADERS)
+        soup = bs.BeautifulSoup(response.text, 'html5lib')
+    except:
+        soup = None
+    return soup
 
-# soup = bs.BeautifulSoup(RESPONSE.text, 'lxml')
-soup = bs.BeautifulSoup(RESPONSE.html.html, 'html5lib')
 
-BOOK_URL = 'https://us.nicebooks.com' + soup.find('a', attrs={'class': 'title'}).get('href')
-RESPONSE = SESSION.get(BOOK_URL, headers=HEADERS)
+def get_element_text(soup: bs.BeautifulSoup, tag, itemprop):
+    element = soup.find(tag, attrs={'itemprop': itemprop})
+    if element:
+        return element.text
+    return None
 
-soup = bs.BeautifulSoup(RESPONSE.text, 'html5lib')
 
-title = soup.find('span', attrs={'itemprop': 'name'})
-if title:
-    print(f'Title: {title.text}')
+def get_element_content(soup: bs.BeautifulSoup, tag, itemprop):
+    try:
+        element = soup.find(tag, attrs={'itemprop': itemprop}).get('content')
+    except:
+        element = None
+    return element
 
-sub_title = soup.find('small', attrs={'itemprop': 'alternativeHeadline'})
-if sub_title:
-    print(f'Sub-Title: {sub_title.text.strip()}')
 
-author = soup.find(itemprop='author')
-if author:
-    print(f'Autor: {author.span.text}')
+def get_author(soup: bs.BeautifulSoup):
+    author = soup.find(itemprop='author')
+    if author:
+        return author.span.text
+    return None
 
-publisher = soup.find('a', attrs={'itemprop': 'publisher'})
-if publisher:
-    print(f'Publisher: {publisher.text}')
 
-try:
-    publication_date = soup.find('div', attrs={'itemprop': 'datePublished'}).get('content')
-except:
-    publication_date = None
-if publication_date:
-    print(f'Publication Date: {publication_date}')
+def get_book_format(soup: bs.BeautifulSoup):
+    book_format = soup.find('link', attrs={'itemprop': 'bookFormat'})
+    if book_format:
+        book_format = str(book_format.find_parent("div").text.strip())
+        return book_format.replace('Format:', '')
+    return None
 
-try:
-    language_iso = soup.find('div', attrs={'itemprop': 'inLanguage'}).get('content')
-except:
-    language_iso = None
-if language_iso:
-    print(f'Language ISO: {language_iso}')
 
-language = soup.find('div', attrs={'itemprop': 'inLanguage'})
-if language:
-    print(f'Language: {language.text}')
+def get_isbn_dict(soup: bs.BeautifulSoup):
+    result = dict()
+    isbn_list = soup.find_all('div', attrs={'itemprop': 'isbn'})
+    if isbn_list:
+        for isbn in isbn_list:
+            if isbn.find_previous_sibling('div').text == 'ISBN-10':
+                isbn_10 = isbn.text
+                print(f'ISBN 10: {isbn_10}')
+                result.update({'isbn-10': isbn_10})
+            else:
+                isbn_13 = isbn.text
+                print(f'ISBN 13: {isbn_13}')
+                result.update({'isbn-13': isbn_13})
+    return result
 
-book_format = soup.find('link', attrs={'itemprop': 'bookFormat'})
-if book_format:
-    print(f'Book Format: {book_format.find_parent("div").text.strip()}')
 
-nof_pages = soup.find('div', attrs={'itemprop': 'numberOfPages'})
-if nof_pages:
-    print(f'Number of pages: {nof_pages.text}')
+def get_image(soup: bs.BeautifulSoup, isbn_13: str):
+    try:
+        image_url = soup.find('img', attrs={'itemprop': 'image'}).get('src')
+    except:
+        image_url = None
+    if image_url:
+        remote_image = SESSION.get(image_url)
+        with open(f'flask_collection/static/cover_images/{isbn_13}.jpg', 'wb') as local_image:
+            local_image.write(remote_image.content)
+        image_name = local_image.name[37:]
+        print(f'Image name: {image_name}')
+        image_url = image_url[:image_url.rfind('?')]
+        remote_image = SESSION.get(image_url)
+        with open(f'flask_collection/static/cover_images/{isbn_13}_large.jpg', 'wb') as large_image:
+            large_image.write(remote_image.content)
 
-isbn_list = soup.find_all('div', attrs={'itemprop': 'isbn'})
-if isbn_list:
-    for isbn in isbn_list:
-        if isbn.find_previous_sibling('div').text == 'ISBN-10':
-            isbn_10 = isbn.text
-            print(f'ISBN 10: {isbn_10}')
-        else:
-            isbn_13 = isbn.text
-            print(f'ISBN 13: {isbn_13}')
 
-# TODO: check that isbn_13 isn't None
-try:
-    image_url = soup.find('img', attrs={'itemprop': 'image'}).get('src')
-except:
-    image_url = None
-if image_url:
-    remote_image = SESSION.get(image_url)
-    with open(f'images/{isbn_13}.jpg', 'wb') as local_image:
-        local_image.write(remote_image.content)
-    image_name = local_image.name[7:] # if folder name is images
-    print(f'Image name: {image_name}')
-    image_url = image_url[:image_url.rfind('?')]
-    remote_image = SESSION.get(image_url)
-    with open(f'images/{isbn_13}_large.jpg', 'wb') as large_image:
-        large_image.write(remote_image.content)
+if __name__ == '__main__':
+    retries = 0
+    book_soup = get_soup()
+    while book_soup is None and retries < 3:
+        book_soup = get_soup()
+        retries += 1
 
-description = soup.find('p', attrs={'itemprop': 'description'})
-if description:
-    print(f'Description: {description.text}')
+    if book_soup:
+        # book_title = get_title(book_soup)
+        book_title = get_element_text(book_soup, 'span', 'name')
+        print(f'Title: {book_title}')
+        book_sub_title = get_element_text(book_soup, 'small', 'alternativeHeadline')
+        print(f'Sub-Title: {book_sub_title}')
+        book_author = get_author(book_soup)
+        print(f'Autor: {book_author}')
+        book_publisher = get_element_text(book_soup, 'a', 'publisher')
+        print(f'Publisher: {book_publisher}')
+        book_publication_date = get_element_content(book_soup, 'div', 'datePublished')
+        print(f'Publication Date: {book_publication_date}')
+        book_language_iso = get_element_content(book_soup, 'div', 'inLanguage')
+        print(f'Language ISO: {book_language_iso}')
+        book_language = get_element_text(book_soup, 'div', 'inLanguage')
+        print(f'Language: {book_language}')
+        book_format = get_book_format(book_soup)
+        print(f'Book Format: {book_format}')
+        book_nof_pages = get_element_text(book_soup, 'div', 'numberOfPages')
+        print(f'Number of pages: {book_nof_pages}')
+        isbn_dict = get_isbn_dict(book_soup)
+        book_description = get_element_text(book_soup, 'p', 'description')
+        print(f'Description: {book_description}')
+        if 'isbn-13' in isbn_dict.keys():
+            get_image(book_soup, isbn_dict['isbn-13'])
